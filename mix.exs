eda179f97fb75c6dd21ba480b5c0fe66468f7abc
Damir Gainetdinov <damir.gainetdinov@iziteq.com>
2015-11-14 23:31:32 +0100

    Release v0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExMarshal.Mixfile do
 
   def project do
     [app: :ex_marshal,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      deps: deps,
      description: description,
